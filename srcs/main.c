27eabdf719746022960f82268e480bd3109d9bde
jon-finkel <nfinkel@student.42.fr>
2018-03-23 23:44:53 +0100

    Stuff.

--- srcs/main.c
+++ srcs/main.c
@@ -6,15 +6,15 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/13 23:24:23 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/03/23 17:31:15 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/03/23 23:39:44 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-#define WIN_TITLE "FdF - A wireframe renderer, by Jon Finkel"
 #define WIN_X 1200
 #define WIN_Y 600
+#define WIN_TITLE "FdF - A wireframe renderer, by Jon Finkel"
 
 static void			parse(t_fdf *fdf, t_vary *vary, char **file)
 {
@@ -31,7 +31,7 @@ static void			parse(t_fdf *fdf, t_vary *vary, char **file)
 		while (*s)
 		{
 			z = ft_atoi(s);
-			*(t_vec4 **)ft_varypush(vary) = ft_vecnew((double)++p * 50, (double)k * 50, (double)z * 50, 1);
+			*(t_vec4 **)ft_varypush(vary) = ft_vecnew(++p, k, z, true);
 			s += ft_intlen(z);
 			if (!IS_WHITESPACE(*s))
 				fdf_errhdl(file[k], k);
@@ -99,6 +99,30 @@ void			ft_bresenham2(t_mlx *mlx, t_vec4 *vec1, t_vec4 *vec2, int color)
 	}
 }
 
+static void			make_iso(t_fdf fdf)
+{
+	double		scale;
+	int			k;
+	t_m4		m1;
+	t_m4		m2;
+
+	scale = WIN_Y / MAX(fdf.width, fdf.height);
+	ft_m4scale(&m2, scale, scale, scale);
+	ft_m4trans(&m1, (WIN_X - ((fdf.width - 1) * scale)) / 2,\
+		(WIN_Y - ((fdf.height - 1) * scale)) / 2, 0);
+	ft_m4_m4(&m1, &m2);
+	ft_m4rotz(&m2, 3.14/4.0);
+		ft_printf("%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n%d, %d, %d, %d\n", m2.a, m2.b, m2.c, m2.d, m2.e, m2.f, m2.g, m2.h, m2.i, m2.j, m2.k, m2.l, m2.m, m2.n, m2.o, m2.p);
+	k = -1;
+	while (++k < fdf.width * fdf.height)
+	{
+//		ft_m4_v4(&m1, fdf.vec[k]);
+		ft_printf("B = %f, %f, %f, %f\n", fdf.vec[k]->x, fdf.vec[k]->y, fdf.vec[k]->z, fdf.vec[k]->w);
+		ft_m4_v4(&m2, fdf.vec[k]);
+		ft_printf("A = %f, %f, %f, %f\n", fdf.vec[k]->x, fdf.vec[k]->y, fdf.vec[k]->z, fdf.vec[k]->w);
+	}
+}
+
 static void			do_wireframe(t_fdf fdf)
 {
 	int		k;
@@ -123,6 +147,7 @@ int					main(int argc, const char *argv[])
 	get_data(&fdf, open(argv[1], O_RDONLY));
 	ft_mlxinit(&_MLX);
 	ft_mlxaddwin(&_MLX, WIN_X, WIN_Y, WIN_TITLE);
+	make_iso(fdf);
 	do_wireframe(fdf);
 	mlx_loop(_MLX.mlx);
 	KTHXBYE;
